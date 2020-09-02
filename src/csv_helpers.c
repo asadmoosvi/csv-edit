0517c48a2cd2f4897679d254dc84de767988710c
Asad Moosvi <moosvi.asad@gmail.com>
2020-09-03 02:43:10 +0500

    Add string slicing function as a helper

--- src/csv_helpers.c
+++ src/csv_helpers.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 void strip_nl(char *s) {
   char *c;
@@ -10,4 +11,15 @@ void strip_nl(char *s) {
 
 void print_error(const char *func_name, const char *msg) {
   fprintf(stderr, "~> ERROR (%s): %s\n", func_name, msg);
+}
+
+char *str_slice(const char *string, int start, int end) {
+  char *slice = malloc(end - start + 1);
+  char *ptr = slice;
+
+  for (int i = start; i < end; i++)
+    *ptr++ = string[i];
+  *ptr = '\0';
+
+  return slice;
 }
--- src/csv_helpers.h
+++ src/csv_helpers.h
@@ -1,2 +1,3 @@
 void strip_nl(char *s);
 void print_error(const char *func_name, const char *msg);
+char *str_slice(const char *string, int start, int end);
