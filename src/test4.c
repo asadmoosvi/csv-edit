bb031e237bf5beb068cff88eb8a9e0e74573bfef
Asad Moosvi <moosvi.asad@gmail.com>
2020-09-05 03:51:38 +0500

    Add test4 for csv_reader_print_table

---
+++ src/test4.c
@@ -0,0 +1,15 @@
+#include "csv_reader.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s csv_filename\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  csv_reader_t *reader = init_csv_reader(argv[1], true);
+  csv_reader_print_table(reader);
+  csv_reader_cleanup(reader);
+  exit(EXIT_SUCCESS);
+}
